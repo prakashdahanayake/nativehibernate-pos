6f64ec9a2251cf168fd22b5d4106b78e38224876
prakashlk <prakash.dahanayake@gmail.com>
2019-05-31 22:43:09 +0530

    Native Hibernate POS

---
+++ README.md
@@ -0,0 +1,6 @@
+# Modern POS - Native Hibernate
+This is a sample CRUD Application with native hibernate bootstrapping.
+
+## License
+
+This project has been licensed [MIT](License).
---
+++ src/lk/ijse/pos/dao/custom/impl/ItemDAOImpl.java
@@ -0,0 +1,9 @@
+package lk.ijse.pos.dao.custom.impl;
+
+import lk.ijse.pos.dao.CrudDAOImpl;
+import lk.ijse.pos.dao.custom.ItemDAO;
+import lk.ijse.pos.entity.Item;
+
+public class ItemDAOImpl extends CrudDAOImpl<Item, String> implements ItemDAO {
+
+}
